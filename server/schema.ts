cae1bc7573ab5b5b859e4f65179f8abfff3c717a
Marc Mansour <marcm9387@gmail.com>
2024-05-22 22:33:42 +0200

    Update npm dependencies and add emailTokens table to schema

--- server/schema.ts
+++ server/schema.ts
@@ -7,14 +7,16 @@ import {
   boolean,
   pgEnum,
 } from "drizzle-orm/pg-core";
-// import { createId } from "@paralleldrive/cuid2"
+import { createId } from "@paralleldrive/cuid2";
 import type { AdapterAccountType } from "next-auth/adapters";
 
 export const RoleEnum = pgEnum("roles", ["user", "admin"]);
 
 export const users = pgTable("user", {
-  id: text("id").notNull().primaryKey(),
-  // .$defaultFn(() => createId()),
+  id: text("id")
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => createId()),
   name: text("name"),
   email: text("email").notNull(),
   emailVerified: timestamp("emailVerified", { mode: "date" }),
@@ -46,5 +48,20 @@ export const accounts = pgTable(
     compoundKey: primaryKey({
       columns: [account.provider, account.providerAccountId],
     }),
+  })
+);
+
+export const emailTokens = pgTable(
+  "email_tokens",
+  {
+    id: text("id")
+      .notNull()
+      .$defaultFn(() => createId()),
+    token: text("token").notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull(),
+    email: text("email").notNull(),
+  },
+  (vt) => ({
+    compoundKey: primaryKey({ columns: [vt.id, vt.token] }),
   })
 );
